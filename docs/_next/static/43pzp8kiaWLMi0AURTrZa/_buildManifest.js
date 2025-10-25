fe9560ae587b9a8687249e47e37076f3c33f66ab
Tien Phan <tienphan@mylong.io>
2025-10-25 22:26:52 +0700

    Build and deploy wedding website - 2025-10-25 22:26:52

---
+++ docs/_next/static/43pzp8kiaWLMi0AURTrZa/_buildManifest.js
@@ -0,0 +1,14 @@
+self.__BUILD_MANIFEST = {
+  "/_error": [
+    "./static/chunks/6a473fdb88a9f2ca.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
