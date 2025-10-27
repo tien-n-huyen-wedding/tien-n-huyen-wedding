359f44473b394f4b711001ee949d38bcb513f9c2
Tien Phan <tienphan@mylong.io>
2025-10-27 08:48:31 +0700

    update logo

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -15,8 +15,8 @@ interface LogoProps {
 export default function Logo({
   src = '/images/main-qr-code.png',
   alt = 'Wedding',
-  width = 100,
-  height = 100,
+  width = 50,
+  height = 50,
   className = '',
   invitationProps = {}
 }: LogoProps) {
