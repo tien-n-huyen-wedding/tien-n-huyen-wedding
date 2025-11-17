0580e80959bd1c7ed9343e2146c0252d6e3bd313
Tien Phan <tienphan@mylong.io>
2025-11-17 14:57:38 +0700

    fix text

--- src/components/main_page_sections/BankAndMapSection.tsx
+++ src/components/main_page_sections/BankAndMapSection.tsx
@@ -30,7 +30,7 @@ export default function BankAndMapSection({
             <span>Support & Directions</span>
             <h2>{title}</h2>
             <p>Mọi tình cảm và lời chúc phúc từ {guestName} đều là món quà vô giá đối với {coupleGreeting}. Nếu {guestName} muốn gửi quà mừng online, vui lòng tham khảo thông tin ngân hàng bên dưới. Ngoài ra, {coupleGreeting} cũng đã gắn link và mã QR Google Maps để chỉ dẫn đến nhà hàng tiệc cưới. {guestNameSentenceCase} hãy nhớ rằng, sự hiện diện của {guestName} chính là món quà lớn nhất với {coupleGreeting} trong ngày trọng đại ấy ❤</p>
-          </div>admin
+          </div>
         </div>
         <div className="row">
           {/* Groom's Bank */}
